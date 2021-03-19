af36495acea509fefa506f3923ae7f299ef8e2a7
Maciej Roszyk <maciej.roszyk@gmail.com>
2021-03-19 12:18:03 +0100

    MR/firstCommit

---
+++ src/main/java/pl/zdajegzamin/driverswebsite/DriversWebsiteApplication.java
@@ -0,0 +1,13 @@
+package pl.zdajegzamin.driverswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriversWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriversWebsiteApplication.class, args);
+    }
+
+}
